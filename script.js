d8306f19d52cb8744fbbd5ef8b98f29f02a95486
UnisBock <bockarieunisa@gmail.com>
2020-04-07 16:18:01 +0300

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+
+    document.getElementById("btn-ourSpells").onclick = function () {
+        location.href = "lovespells.html";
+    };
+    document.getElementById("btn-contactUs").onclick = function () {
+        location.href = "contact.html";
+    };
+
+    
+    document.getElementById("btn-ourSpells2").onclick = function () {
+        location.href = "lovespells.html";
+    };
+    document.getElementById("btn-contactUs2").onclick = function () {
+        location.href = "contact.html";
+    };
+
+    document.getElementById("btn-ourSpells3").onclick = function () {
+        location.href = "lovespells.html";
+    };
+    document.getElementById("btn-contactUs3").onclick = function () {
+        location.href = "contact.html";
+    };
